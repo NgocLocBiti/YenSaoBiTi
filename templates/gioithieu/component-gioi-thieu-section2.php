159ae489c064f202a3e9b553f66642d9cff3ca9c
VanDang10 <dangtran.031000@gmail.com>
2023-09-04 14:09:27 +0700

    add

---
+++ templates/gioithieu/component-gioi-thieu-section2.php
@@ -0,0 +1,22 @@
+<section class="introduce-yen">
+    <div class="container px-2 px-lg-5">
+        <div class="row">
+            <div class="col-md-6 col-12">
+                <figure class="introduce-img">
+                    <img src="https://bitishop.site/yensao/wp-content/uploads/2023/08/Yen_Sao_Dai_An-01-1.png" alt="">
+                </figure>
+            </div>
+            <div class="col-md-6 col-12">
+                <div class="content">
+                    <h2 class="title mb-0">
+                        YẾN SÀO ĐẠI AN </h2>
+                    <h3 class="sub-title mb-4">
+                        Sức khoẻ vàng trao ngàn yêu thương </h3>
+                    <p class="comment">
+                        Nguyên vẹn những giá trị từ Yến, Yến sào Đại An mang đến cho người dùng những sản phẩm Yến chất lượng!<br>
+                        Với nhiều năm trong lĩnh vực sản phẩm chăm sóc sức khỏe cho người dùng Việt Nam, Yến sào Đại An dần góp phần không nhỏ vào sự bảo vệ sức khỏe và nâng cao chất lượng cuộc sống.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
