d33631f9cbb26344ae110f07d9830fb040d11498
= <=>
2023-09-04 08:24:02 +0700

    add

--- index.php
+++ index.php
@@ -10,14 +10,6 @@
     <?php get_template_part('templates/block/component', 'home-section-six'); ?>
     <?php get_template_part('templates/block/component', 'home-section-seven'); ?>
     <?php get_template_part('templates/block/component', 'home-section-eight'); ?>
-<<<<<<< HEAD
-=======
-
-    
-    <?php get_template_part('templates/block/component', 'home-section-test'); ?>
-
-
->>>>>>> d2f1e2f8516527a2f1d94fe9fd2a5a0a20462391
     <?php get_template_part('templates/block/component', 'home-section9'); ?>
     <?php get_template_part('templates/block/component', 'home-section10'); ?>
     <?php get_template_part('templates/block/component', 'home-section11'); ?>
