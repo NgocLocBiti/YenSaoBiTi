7b8dbf4cde012b726a3bbd4f3db7174aa52b5baf
= <=>
2023-09-05 09:13:52 +0700

    loccccc

--- index.php
+++ index.php
@@ -18,3 +18,6 @@
 
 
 <?php get_footer(); ?>
+
+
+<?php get_template_part('templates/block/component', 'home-to-top'); ?>
---
+++ templates/Gioi-thieu-ve-chung-toi/component-vechungtoi-section1.php
@@ -0,0 +1,9 @@
+<div class="vechungtoi-section1">
+    <div class="container">
+        <div class="breadcrumbs">
+            <a href="">Trang chủ</a>
+            /
+            <a href="">Về chúng tôi</a>
+        </div>
+    </div>
+</div>
